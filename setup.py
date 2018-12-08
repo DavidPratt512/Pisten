c525226edac6e1ee240adbf780464dddb4841ab3
David Pratt <DavidPratt512@gmail.com>
2018-12-08 14:48:51 -0700

    adjustments to setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 with open('README.md', 'r') as f:
-    long_description = f.read()
+    readme = f.read()
 
 setuptools.setup(
         name='pisten',
@@ -9,17 +9,23 @@ setuptools.setup(
         author='David Pratt',
         author_email='davidpratt512@gmail.com',
         description='A simple magic packet forwarder',
-        long_description=long_description,
+        license='MIT',
+        long_description=readme,
         long_description_content_type='text/markdown',
-        url='https://githup.com/davidpratt512/pisten',
+        keywords='wakeonlan wake-on-lan port-forwarding magic-packet',
+        python_requires='>=3',
+        url='https://github.com/davidpratt512/pisten',
         packages=setuptools.find_packages(),
         classifiers=[
             'Development Status :: 4 - Beta',
             'Intended Audience :: System Administrators',
-            'Topic :: Internet',
+            'Topic :: System :: Networking',
             'Programming Language :: Python :: 3 :: Only',
             'License :: OSI Approved :: MIT License',
             'Operating System :: OS Independent'
-            ]
+            ],
+        entry_points={
+            'console_scripts': ['pisten = pisten:main']
+        }
 )
 
