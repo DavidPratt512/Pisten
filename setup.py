bc228e7d5673761bc2113c277a474c9079cf4fe4
David Pratt <DavidPratt512@gmail.com>
2018-12-08 15:34:25 -0700

    change tag to 0.1.1 and reflect change in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
         name='pisten',
-        version='0.1',
+        version='0.1.1',
         author='David Pratt',
         author_email='davidpratt512@gmail.com',
         description='A simple magic packet forwarder',
