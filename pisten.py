f2a5d35bf0ed84b2a0a955755b6d3fa9114140fd
David Pratt <DavidPratt512@gmail.com>
2018-12-08 00:49:41 -0700

    much needed infinite loop fixing

--- pisten.py
+++ pisten.py
@@ -2,8 +2,9 @@ import argparse
 import socket
 
 
-DEFAULT_PORT = 9
-DEFAULT_MACS = [None]
+DEFAULT_LISTEN_PORT = 1729
+DEFAULT_TARGET_PORT = 9
+DEFAULT_TARGET_IP = '255.255.255.255'
 
 
 def extract_mac(data):
@@ -33,46 +34,62 @@ def extract_mac(data):
 
 
 def listen(**kwargs):
-    port = kwargs.pop('port', DEFAULT_PORT)
-    macs = kwargs.pop('macs', DEFAULT_MACS)
+    """
+    Listens for activity on UDP port specified by listen_port and
+    forwards WOL packets to (target_ip, target_port).
+
+    """
+    listen_port = kwargs.pop('listen_port', DEFAULT_LISTEN_PORT)
+    target_port = kwargs.pop('target_port', DEFAULT_TARGET_PORT)
+    target_ip = kwargs.pop('target_ip', DEFAULT_TARGET_IP)
+    
     for kw in kwargs:
         raise TypeError('Unexpected keyword argument: {!r}'.format(kw))
 
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.bind(('', port))
+    s.bind(('', listen_port))
     while True:
         data, _ = s.recvfrom(1024)
-        mac = extract_mac(data)
-        if mac not in macs:
-            #forward(data)
-            pass
-
-
+        if extract_mac(data):
+            forward(data, (target_ip, target_port))
 
 
 def main(argv=None):
     parser = argparse.ArgumentParser(
-            description='Forward wake-on-lan packets to the broadcast IP address.'
+            description='Forward wake-on-lan packets to the '
+            'broadcast IP address.'
     )
-    parser.add_argument('-p',
-            metavar='port',
-            default=DEFAULT_PORT,
-            dest='port',
+    parser.add_argument('-L',
+            metavar='listen port',
+            default=DEFAULT_LISTEN_PORT,
+            dest='L',
             nargs=1,
             type=int,
-            help='The UDP port number you want to listen to (default 9).'
+            help='The UDP port number you want to listen to '
+            '(default 1729).'
     )
-    parser.add_argument('-m',
-            metavar='macs',
-            default=DEFAULT_MACS,
-            nargs='+',
-            dest='macs',
-            help='A list of MAC addresses that you do not wish to wake. '
-            'By default, all wake-on-lan packets will be broadcasted.'
+    parser.add_argument('-F',
+            metavar='forward port',
+            default=DEFAULT_TARGET_PORT,
+            dest='F',
+            nargs=1,
+            type=int,
+            help='The UDP port number you want to forward WOL '
+            'packets to (default 9).'
+    )
+    parser.add_argument('-I',
+            metavar='forward IP',
+            default=DEFAULT_TARGET_IP,
+            dest='I',
+            nargs=1,
+            help='The IP address you want to forward WOL packets '
+            'to (default 255.255.255.255).'
     )
     args = parser.parse_args(argv)
     print(args)
-    listen(macs=args.macs, port=args.port)
+    listen(listen_port=args.L,
+            target_port=args.F,
+            target_ip=args.I)
 
 
 if __name__ == '__main__':
