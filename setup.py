f2a7d7c8d69414ae66e152083c70cfa76c28e35e
David Pratt <DavidPratt512@gmail.com>
2018-12-08 17:01:56 -0700

    update version 0.2.0 in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
         name='pisten',
-        version='0.1.2',
+        version='0.2.0',
         author='David Pratt',
         author_email='davidpratt512@gmail.com',
         description='A simple magic packet forwarder',
